7018e91ab9d0e8339b965cb6be64d2a8ca18b66a
DearYuto <reasons@kakao.com>
2024-03-12 01:47:23 +0900

    chore: header, footer 컴포넌트 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
 import './App.css';
-import { PATH } from './components/routes/constants/path';
+
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
-      <h1>사이드바</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to={PATH.root}>메인</Link>
-          </li>
-          <li>
-            <Link to={PATH.posts}>전체 포스트</Link>
-          </li>
-          <li>
-            <Link to={PATH.mypage}>마이페이지</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
