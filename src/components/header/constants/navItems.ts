19d4c6bae7bb3c92d39fa2cb032748ec1eeceafa
DearYuto <reasons@kakao.com>
2024-03-13 03:44:32 +0900

    feat: role에 따라 nav 메뉴 분리

--- src/components/header/constants/navItems.ts
+++ src/components/header/constants/navItems.ts
@@ -3,17 +3,20 @@ import { PATH } from '@/components/router/constants/path';
 export const NAV_ITEMS = [
   {
     id: 0,
-    title: '메인',
-    path: PATH.root,
+    title: '로그인',
+    path: PATH.login,
+    role: 'none',
   },
   {
     id: 1,
-    title: '포스트',
-    path: PATH.posts,
+    title: '회원가입',
+    path: PATH.join,
+    role: 'none',
   },
   {
     id: 2,
     title: '마이페이지',
     path: PATH.mypage,
+    role: 'user',
   },
 ] as const;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,14 +4,16 @@ import { NAV_ITEMS } from './constants/navItems';
 import { PATH } from '../router/constants/path';
 
 export default function Header() {
+  const nonMemberMenu = NAV_ITEMS.filter((item) => item.role === 'none');
+
   return (
     <header className="header">
       <nav className="header__nav">
         <h1 className="header__title">
-          <Link to={PATH.root}>유토의 하루</Link>
+          <Link to={PATH.root}>U-log</Link>
         </h1>
         <ul className="header__nav--list">
-          {NAV_ITEMS.map((nav) => {
+          {nonMemberMenu.map((nav) => {
             return (
               <li key={nav.id} className="header__nav-item">
                 <Link to={nav.path}>{nav.title}</Link>
