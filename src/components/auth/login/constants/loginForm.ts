2818b6f158da7c671d446d8e7ae10ebddaa071db
DearYuto <reasons@kakao.com>
2024-03-17 00:52:36 +0900

    feat: 로그인 폼 상수 오브젝트 내부 값 추가

--- src/components/auth/login/constants/loginForm.ts
+++ src/components/auth/login/constants/loginForm.ts
@@ -1,13 +1,17 @@
-export const LOGIN_FORM = [
+import { FormInputType } from '@/types/auth/form';
+
+export const LOGIN_FORM: FormInputType[] = [
   {
     id: 0,
     name: 'email',
+    label: '이메일',
     type: 'email',
-    placeholder: '이메일을  입력해주세요.',
+    placeholder: '이메일을 입력해주세요.',
   },
   {
     id: 1,
     name: 'password',
+    label: '이메일',
     type: 'password',
     placeholder: '비밀번호를 입력해주세요.',
   },
