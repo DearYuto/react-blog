769393eddf5170c5ca71f284974a4917813a7ea4
DearYuto <reasons@kakao.com>
2024-03-17 21:18:56 +0900

    feat: 로딩 스피너 컴포넌트 추가

---
+++ src/components/loading/Spinner.tsx
@@ -0,0 +1,10 @@
+export default function Spinner() {
+  return (
+    <>
+      <div className="spinner">
+        <div className="spinner__object"></div>
+        <p className="spinner__text">잠시만 기다려주세요.</p>
+      </div>
+    </>
+  );
+}
