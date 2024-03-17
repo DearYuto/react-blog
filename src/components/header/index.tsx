2f8f833a901b24b7965136a9dc376ff73146cb09
DearYuto <reasons@kakao.com>
2024-03-17 19:44:37 +0900

    design: 로고 이미지 및 파비콘 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,7 +10,10 @@ export default function Header() {
     <header className="header">
       <nav className="header__nav">
         <h1 className="header__title">
-          <Link to={PATH.root}>U-log</Link>
+          <Link className="header__logo" to={PATH.root}>
+            <img src="blog-logo.svg" alt="logo" />
+            <span aria-label="u-log">U-LOG</span>
+          </Link>
         </h1>
         <ul className="header__nav--list">
           {nonMemberMenu.map((nav) => {
