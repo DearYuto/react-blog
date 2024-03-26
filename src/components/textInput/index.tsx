c932e07adef90f89cb9ee23eccf0bf3a9329c220
DearYuto <reasons@kakao.com>
2024-03-27 02:51:36 +0900

    chore: TextInput 컴포넌트 프롭스 value 추가

--- src/components/textInput/index.tsx
+++ src/components/textInput/index.tsx
@@ -1,17 +1,19 @@
 type Props = {
   labelFor: string;
   label: string;
+  value?: string;
   placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 };
 
-export default function TextInput({ label, labelFor, placeholder, onChange }: Props) {
+export default function TextInput({ label, value, labelFor, placeholder, onChange }: Props) {
   return (
     <>
       <label className="visually-hidden" htmlFor={labelFor}>
         {label}
       </label>
       <textarea
+        value={value}
         className={`textarea write__textarea--${labelFor}`}
         onChange={onChange}
         id={labelFor}
